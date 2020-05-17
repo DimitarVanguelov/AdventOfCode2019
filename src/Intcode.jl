f8f5679d230f9b721033f5d521a2e6d67e8fabba
Dimitar Vanguelov <dvanguelov@gmail.com>
2020-05-17 13:16:45 -0400

    Day 2 part 1 solution, with some code I might want, but not final.

---
+++ src/Intcode.jl
@@ -0,0 +1,48 @@
+v = [1,9,10,3,2,3,11,0,99,30,40,50]
+len = length(v)
+p = [0:len-1;] # position helper
+
+function calc(opcode, input1, input2)
+    if opcode == 1
+        result = input1 + input2
+    elseif opcode == 2
+        result = input1 * input2
+    else
+        throw("Unknown operation!")
+    end
+    return result
+end
+
+adj_v = []
+for n in p
+    push!(adj_v, (n, v[n+1]))
+end
+
+
+function intcode(v::Vector{Int64})
+    new_v = copy(v)
+    len = length(v)
+
+    for i in 1:4:len
+        # i = n + 1
+        if new_v[i] == 99
+            break
+        else
+            # println((n, v[i]))
+            # a = (v[i], v[i+1], v[i+2])
+            op = new_v[i]
+
+            input1_pos = new_v[i+1] + 1
+            input2_pos = new_v[i+2] + 1
+
+            input1_val = new_v[input1_pos]
+            input2_val = new_v[input2_pos]
+
+            res_pos = new_v[i+3] + 1
+            res_val = calc(op, input1_val, input2_val)
+
+            new_v[res_pos] = res_val
+        end # if
+    end # loop
+    return new_v
+end
