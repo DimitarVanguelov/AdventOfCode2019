6d3fb7ed886c5263aac925fb73a10c8c6bbe9d3c
Dimitar Vanguelov <dvanguelov@gmail.com>
2020-05-17 15:12:58 -0400

    Added unit tests for Intcode program (Day 2, Part 1).

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using AdventOfCode2019
+import AdventOfCode2019: intcode_calc
 using Test
 
 @testset "AoC 2019 - Day01" begin
@@ -14,4 +15,15 @@ using Test
     @test actual_fuel(14) == 2
     @test actual_fuel(1969) == 966
     @test actual_fuel(100756) == 50346
+end
+
+@testset "AoC 2019 - Day02" begin
+    # Day 2, Part 1 examples
+    @test intcode_calc(1, 2, 3) == 5
+    @test intcode_calc(2, 2, 3) == 6
+    @test_throws ErrorException intcode_calc(3, 2, 3)
+
+    input_vector = [1,9,10,3,2,3,11,0,99,30,40,50]
+    output_vector = [3500,9,10,70,2,3,11,0,99,30,40,50]
+    @test intcode(input_vector) == output_vector
 end
